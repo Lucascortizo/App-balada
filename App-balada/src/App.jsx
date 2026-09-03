a4356f0256b1f0fe86193a9e8deb8205ab4e5f7e
Lucascortizo <lucascortizo768@gmail.com>
2026-09-02 21:13:56 -0300

    Preparando para produção

--- App-balada/src/App.jsx
+++ App-balada/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthProvider, AuthContext } from './contexts/AuthContext';
 import { Toaster } from 'react-hot-toast';
